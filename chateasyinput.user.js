c04a30184f2f935ed7c6896f8887c0e237428417
rlemon <rob.lemon@gmail.com>
2012-02-20 20:03:27 -0500

    cleanup and added input_link

--- chateasyinput.user.js
+++ chateasyinput.user.js
@@ -21,39 +21,85 @@ function EmbedFunctionOnPageAndExecute(fn) {
 
 /* Main */
 EmbedFunctionOnPageAndExecute(function() {
-
+	
 	/* Chat Input Textarea */
 	var chat_input = document.getElementById('input');
 
+	/* utility functions */
+	var get_selection = function() {
+		return chat_input.value.substring( chat_input.selectionStart, chat_input.selectionEnd );
+	}
+	var get_pre = function() {
+		return chat_input.value.substring(0, chat_input.selectionStart);
+	}
+	var get_post = function() {
+		return chat_input.value.substring(chat_input.selectionEnd, chat_input.value.length);
+	}
+	var in_input = function() {
+		return document.activeElement === chat_input;
+	}
+	
+	
+
 	/* ALT + T handler 
-	 * If focus is in the textarea insert the '[tag:]' at the caret position
+	 * If focus is in the textarea AND NO selection range is set, insert the '[tag:]' at the caret position
 	 * and move the caret just past the colon.
-	 * If focus is not in the textarea insert the '[tag:]' at the end of the textarea
-	 * and move the caret just past the colon.
+	 * If focus is in the textarea AND there is a selection range is set, wrap the range in '[tag:' + ']',
+	 * and move the caret just past range end. deselect the range.
+	 * If focus is not in the textarea do nothing.
 	 * */
 	var input_tag = function() {
 		var pos = 0,
-			TAG = '[tag:]';
-		if (document.activeElement == chat_input) {
-			var pre = chat_input.value.substring(0, chat_input.selectionStart) + TAG;
-			var post = chat_input.value.substring(chat_input.selectionEnd, chat_input.value.length);
+			TAG_OPEN = '[tag:',
+			TAG_CLOSE = ']';
+		if ( in_input() ) {
+			var selection = get_selection(), pre = get_pre(), post = get_post();
+			if( selection ) {
+				pre += TAG_OPEN + selection + TAG_CLOSE;	
+			} else {
+				pre += TAG_OPEN;
+				post = TAG_CLOSE + post;
+			}
 			chat_input.value = pre + post;
-			pos = pre.length - 1;
-		} else {
-			chat_input.value = chat_input.value + TAG;
-			pos = chat_input.value.length - 1;
+			pos = pre.length;
+			chat_input.setSelectionRange(pos, pos);
 		}
-		chat_input.setSelectionRange(pos, pos);
+	};
+	
+	/* ALT + A handler 
+	 * If focus is in the textarea AND NO selection range is set, prompt for url. Insert the link at the caret position with the url and default text.
+	 * select default text.
+	 * If focus is in the textarea AND there is a selection range is set, prompt the user for a url to correspond to the link. replace the selection with the link
+	 * and move the caret just past range end. deselect the range.
+	 * If focus is not in the textarea do nothing.
+	 * */
+	var input_link = function() {
+		if ( in_input() ) {
+			var selection = get_selection(), pre = get_pre(), post = get_post(), pos_start = 0, pos_end = 0;
+			var url = prompt('Enter a URL\nhttp://example.com/ "optional title"','http://');
+			if( url === null ) return; // user hit cancel
+			if( selection ) {
+				pre += '[' + selection + '](' + url + ')';
+				pos_start = pos_end = pre.length;
+			} else {
+				pos_start = pre.length + 1;
+				pos_end = pos_start + 27; // probably a cheap way to do this.... 
+				pre += '[enter link description here](' + url + ')';
+			}
+			chat_input.value = pre + post;
+			chat_input.setSelectionRange(pos_start, pos_end);
+		}
 	};
 
 	/* Run the page after the shortcut script finishes loading */
 	var run = function() {
 		shortcut.add('Alt+T', input_tag);
+		shortcut.add('Alt+A', input_link);
 	};
 
 	/* First load the shortcut script */
 	var sc_script = document.createElement('script');
-	sc_script.src = 'http://rlemon.com/public_files/shortcut.min.js';
+	sc_script.src = 'http://rlemon.com/public_files/shortcut.min.js'; // hopefully I will be able to find a better place to host this..... ;) ;)
 	sc_script.onreadystatechange = function() {
 		if (this.readyState == 'complete') run();
 	}
